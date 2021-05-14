cda7a7783fd37025b302f0b8b1a25638a8c8c34a
Henrique <ricogx@hotmail.com>
2021-05-13 22:41:34 -0300

    adicionado suporte jwt no swagger doc

--- ApiGoBarber/Startup.cs
+++ ApiGoBarber/Startup.cs
@@ -58,6 +58,29 @@ namespace ApiGoBarber
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GoBarber API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+                    Description = "Insira o token JET desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
 
             services.AddControllers(options =>
